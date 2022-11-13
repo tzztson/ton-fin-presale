a8621e81ea3e132826d08e3ecdecd86835179e57
tzztson <zhenghu61919@gmail.com>
2022-11-13 19:54:54 +0800

    add: added the import_token func

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ export default function Home() {
   const [FTMamount, setFTMamount] = useState(0);
   const [TFTamount, setTFTamount] = useState(0);
   const [TFTPrice, setTFTPrice] = useState(0);
+
   const wallet = useWallet();
 
   useEffect(() => {
@@ -17,6 +18,40 @@ export default function Home() {
       getPrice();
     }
   }, [wallet.status]);
+
+  useEffect(() => {
+    setTFTamount(FTMamount * TFTPrice);
+  }, [FTMamount]);
+
+  const importTokentoMetamask = async () => {
+    const tokenAddress = contractABI.token.address;
+    const tokenSymbol = "TFT";
+    const tokenDecimals = 18;
+    // const tokenImage =
+    try {
+      // wasAdded is a boolean. Like any RPC method, an error may be thrown.
+      const wasAdded = await ethereum.request({
+        method: "wallet_watchAsset",
+        params: {
+          type: "ERC20", // Initially only supports ERC20, but eventually more!
+          options: {
+            address: tokenAddress, // The address that the token is at.
+            symbol: tokenSymbol, // A ticker symbol or shorthand, up to 5 chars.
+            decimals: tokenDecimals, // The number of decimals in the token
+            // image: tokenImage, // A string url of the token logo
+          },
+        },
+      });
+
+      if (wasAdded) {
+        console.log("Thanks for your interest!");
+      } else {
+        console.log("Your loss!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const getPrice = async () => {
     try {
@@ -35,10 +70,6 @@ export default function Home() {
       console.log(err);
     }
   };
-
-  useEffect(() => {
-    setTFTamount(FTMamount * TFTPrice);
-  }, [FTMamount]);
 
   const buy_button =
     wallet.status == "connected" ? "Buy Token" : "Connect Wallet";
@@ -187,7 +218,10 @@ export default function Home() {
           </button>
         </form>
         <div className="grid gap-y-4 w-lg max-w-full py-10 px-6 mx-auto">
-          <button className="text-black hover:underline font-bold">
+          <button
+            className="text-black hover:underline font-bold"
+            onClick={importTokentoMetamask}
+          >
             Import TFT Token to Metamask
           </button>
         </div>
