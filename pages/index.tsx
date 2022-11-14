432f61dd52d50b966164ee81f53c5bf2497be4d6
tzztson <zhenghu61919@gmail.com>
2022-11-14 20:48:16 +0800

    add: added the @types for ethereum

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,29 +24,33 @@ export default function Home() {
   }, [FTMamount]);
 
   const importTokentoMetamask = async () => {
+    const { ethereum } = window;
     const tokenAddress = contractABI.token.address;
     const tokenSymbol = "TFT";
     const tokenDecimals = 18;
     // const tokenImage =
     try {
-      // wasAdded is a boolean. Like any RPC method, an error may be thrown.
-      const wasAdded = await ethereum.request({
-        method: "wallet_watchAsset",
-        params: {
-          type: "ERC20", // Initially only supports ERC20, but eventually more!
-          options: {
-            address: tokenAddress, // The address that the token is at.
-            symbol: tokenSymbol, // A ticker symbol or shorthand, up to 5 chars.
-            decimals: tokenDecimals, // The number of decimals in the token
-            // image: tokenImage, // A string url of the token logo
+      if (ethereum) {
+        // wasAdded is a boolean. Like any RPC method, an error may be thrown.
+        const wasAdded = await ethereum.request({
+          method: "wallet_watchAsset",
+          params: {
+            type: "ERC20", // Initially only supports ERC20, but eventually more!
+            options: {
+              address: tokenAddress, // The address that the token is at.
+              symbol: tokenSymbol, // A ticker symbol or shorthand, up to 5 chars.
+              decimals: tokenDecimals, // The number of decimals in the token
+              // image: tokenImage, // A string url of the token logo
+            },
           },
-        },
-      });
-
-      if (wasAdded) {
-        console.log("Thanks for your interest!");
+        });
+        if (wasAdded) {
+          console.log("Thanks for your interest!");
+        } else {
+          console.log("Your loss!");
+        }
       } else {
-        console.log("Your loss!");
+        alert("Please install metamask.");
       }
     } catch (error) {
       console.log(error);
@@ -74,7 +78,7 @@ export default function Home() {
   const buy_button =
     wallet.status == "connected" ? "Buy Token" : "Connect Wallet";
 
-  const handleChange = (e):any => {
+  const handleChange = (e: any) => {
     setFTMamount(e.target.value);
   };
 
